660e6410070eee064dda4d75e9a122f639cd0200
zkimsuh <shkim@j2stechno.com>
2021-03-18 20:01:23 +0900

    feat: 프로퍼티 객체 생성
    yaml 파일의 정보를 로드
    테스트코드 작성

---
+++ src/main/java/me/totoku103/magicpacket/magicpacketservicerest/common/MagicPacketInfo.java
@@ -0,0 +1,19 @@
+package me.totoku103.magicpacket.magicpacketservicerest.common;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Getter
+@Setter
+@ToString
+@Component
+@ConfigurationProperties("wol")
+public class MagicPacketInfo {
+    private String broadcastIp;
+    private Map<String, String> macAddresses;
+}
---
+++ src/test/java/me/totoku103/magicpacket/magicpacketservicerest/common/MagicPacketInfoTest.java
@@ -0,0 +1,45 @@
+package me.totoku103.magicpacket.magicpacketservicerest.common;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Map;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.Is.is;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MagicPacketInfoTest {
+
+    @Autowired
+    MagicPacketInfo magicPacketInfo;
+
+    @Test
+    @DisplayName("TargetProperties Bean 생성 여부")
+    public void given_setAutowired_when_applicationLoaded_then_isNotNull() {
+        assertThat(magicPacketInfo, is(not(nullValue())));
+    }
+
+    @Test
+    @DisplayName("broadcastIp 속성 값 존재 여부")
+    public void given_broadcastIp_when_applicationLoaded_then_isNotNull() {
+        final String broadcastIp = magicPacketInfo.getBroadcastIp();
+        assertThat(broadcastIp, is(not(nullValue())));
+        assertThat(broadcastIp, is("192.168.0.255"));
+    }
+
+    @Test
+    @DisplayName("macAddress 리스트 값 존재 여부")
+    public void given_macAddress_when_applicationLoaded_then_isNotNullAndSizeIsNotZero() {
+        final Map<String, String> macAddresses = magicPacketInfo.getMacAddresses();
+        assertThat(macAddresses, is(not(nullValue())));
+        assertThat(macAddresses.size(), is(greaterThan(1)));
+    }
+
+}
