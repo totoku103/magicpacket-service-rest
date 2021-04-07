d7321c4a29e846a68f4ffd8a57c3986add0c028d
zkimsuh <shkim@j2stechno.com>
2021-04-07 22:31:24 +0900

    feat: NotFoundException 추가

---
+++ src/main/java/me/totoku103/magicpacket/magicpacketservicerest/common/NotFoundException.java
@@ -0,0 +1,7 @@
+package me.totoku103.magicpacket.magicpacketservicerest.common;
+
+public class NotFoundException extends RuntimeException{
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
